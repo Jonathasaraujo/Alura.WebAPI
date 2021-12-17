52cdb54170b74a66fd1fc678464d9d575b50e4e5
Jonathas Araújo <31988572+Jonathasaraujo@users.noreply.github.com>
2021-12-17 15:24:59 -0300

    Adicionar arquivos de projeto.

---
+++ Alura.WebAPI.WebApp/Api/LivrosController.cs
@@ -0,0 +1,72 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Alura.WebAPI.WebApp.Api
+{
+    public class LivrosController : ControllerBase
+    {
+        private readonly IRepository<Livro> _repo;
+
+        public LivrosController(IRepository<Livro> repository)
+        {
+            _repo = repository;
+        }
+        [HttpGet]
+        public IActionResult Recuperar(int id)
+        {
+            var model = _repo.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Incluir([FromBody] LivroUpload model) //201
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                _repo.Incluir(livro);
+                var uri = Url.Action("Recuperar", new { id = livro.Id });
+                return Created(uri, livro);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public IActionResult Alterar([FromBody] LivroUpload model) //200
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                if (model.Capa == null)
+                {
+                    livro.ImagemCapa = _repo.All
+                        .Where(l => l.Id == livro.Id)
+                        .Select(l => l.ImagemCapa)
+                        .FirstOrDefault();
+                }
+                _repo.Alterar(livro);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        
+        public IActionResult Remover(int id) //203
+        {
+            var model = _repo.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _repo.Excluir(model);
+            return NoContent();
+        }
+
+    }
+}
